84ea2fbb21a5545a7eb52845f1bf1aca975c95d4
AbdulMalik <abdumaliksharipov@gmail.com>
2020-06-01 23:50:21 +0500

    Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import CurrencyRow from "./CurrencyRow";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
       
     </div>
   );
---
+++ src/CurrencyRow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CurrencyRow() {
+    return (
+        <div>
+            <input type="number" className="input"/>
+            <select>
+                <option>Hi</option>
+            </select>
+            
+        </div>
+    )
+}
